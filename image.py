09158d8407e672238bfbb0df4cbb35b033d19386
Deepak Deedar Singla <47693198+deepakdeedar@users.noreply.github.com>
2020-09-16 21:54:10 +0530

    thumbnail

--- image.py
+++ image.py
@@ -5,3 +5,8 @@ filtered_img = img.convert('L')
 filtered_img.save('grey.png', 'png')
 filtered_img = img.convert('RGB')
 filtered_img.save('rgb.png', 'png')
+
+img2 = Image.open('./astro.jpg')
+img2.thumbnail((400, 400))
+img2.save('thumbnail.png', 'png')
+print(img2.size)
